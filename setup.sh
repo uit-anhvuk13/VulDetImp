099ec72c0e272d2dab6588a776df7bcf6635f1cb
Tran Anh Vu (vuwin11) <anvut20@gmail.com>
2023-12-25 23:48:46 +0700

    do not change the repo's folder name

--- setup.sh
+++ setup.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-git clone https://github.com/uit-anhvuk13/VulDetImp code
-cd code
+git clone https://github.com/uit-anhvuk13/VulDetImp
+cd VulDetImp
 mkdir -p input output
 git clone https://github.com/leontsui1987/VulDetector
 rm -rf VulDetector/.git*
 cp -r src/* VulDetector
-cd input
-wget https://ftp.openssl.org/source/old/1.0.2/openssl-1.0.2.tar.gz
-wget https://ftp.openssl.org/source/openssl-3.2.0.tar.gz
-tar -xvf openssl-1.0.2.tar.gz
-tar -xvf openssl-3.2.0.tar.gz
-rm openssl-1.0.2.tar.gz
-rm openssl-3.2.0.tar.gz
+#cd input
+#wget https://ftp.openssl.org/source/old/1.0.2/openssl-1.0.2.tar.gz
+#wget https://ftp.openssl.org/source/openssl-3.2.0.tar.gz
+#tar -xvf openssl-1.0.2.tar.gz
+#tar -xvf openssl-3.2.0.tar.gz
+#rm openssl-1.0.2.tar.gz
+#rm openssl-3.2.0.tar.gz
