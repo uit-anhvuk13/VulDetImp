ce66215d8409fe59e0e0454f3ccf993e79db219e
Tran Anh Vu (vuldetector) <anvut20@gmail.com>
2023-12-26 21:39:50 +0000

    modify dir structure

--- setup.sh
+++ setup.sh
@@ -4,14 +4,15 @@ set -e
 
 git clone https://github.com/uit-anhvuk13/VulDetImp
 cd VulDetImp
-mkdir -p input output
+mkdir -p DATA/{FUN,CFG,RAW,WFG}
+for i in DATA/*; do mkdir -p $i/{APP,VUL,PAT}; done
 git clone https://github.com/leontsui1987/VulDetector
 rm -rf VulDetector/.git*
 cp -r src/* VulDetector
-#cd input
-#wget https://ftp.openssl.org/source/old/1.0.2/openssl-1.0.2.tar.gz
-#wget https://ftp.openssl.org/source/openssl-3.2.0.tar.gz
-#tar -xvf openssl-1.0.2.tar.gz
-#tar -xvf openssl-3.2.0.tar.gz
-#rm openssl-1.0.2.tar.gz
-#rm openssl-3.2.0.tar.gz
+mkdir -p DATA/RAW/APP/OpenSSL && cd DATA/RAW/APP/OpenSSL
+wget https://ftp.openssl.org/source/old/1.0.2/openssl-1.0.2.tar.gz
+wget https://ftp.openssl.org/source/openssl-3.2.0.tar.gz
+tar -xvf openssl-1.0.2.tar.gz
+tar -xvf openssl-3.2.0.tar.gz
+rm openssl-1.0.2.tar.gz
+rm openssl-3.2.0.tar.gz
