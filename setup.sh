c1e45c79fe2b51239bc95850537cb2769abb90f2
Tran Anh Vu (vuwin11) <anvut20@gmail.com>
2023-12-27 17:05:56 +0700

    add DATA/CVE info
    - DATA/CVE_App.txt: the list of CVEs and affected softwares
    - DATA/CVE_Func.txt: the affected func of each CVE
    
    Thanks the author team of https://github.com/leontsui1987/VulDetector
    for providing the CVE dataset they collected, used and mentioned in
    https://ieeexplore.ieee.org/document/9309254

--- setup.sh
+++ setup.sh
@@ -8,11 +8,4 @@ mkdir -p DATA/{FUN,CFG,RAW,WFG}
 for i in DATA/*; do mkdir -p $i/{APP,VUL,PAT}; done
 git clone https://github.com/leontsui1987/VulDetector
 rm -rf VulDetector/.git*
-cp -r src/* VulDetector
-mkdir -p DATA/RAW/APP/OpenSSL && cd DATA/RAW/APP/OpenSSL
-wget https://ftp.openssl.org/source/old/1.0.2/openssl-1.0.2.tar.gz
-wget https://ftp.openssl.org/source/openssl-3.2.0.tar.gz
-tar -xvf openssl-1.0.2.tar.gz
-tar -xvf openssl-3.2.0.tar.gz
-rm openssl-1.0.2.tar.gz
-rm openssl-3.2.0.tar.gz
+cp -r src/DataPrepare VulDetector/
