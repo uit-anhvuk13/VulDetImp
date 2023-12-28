8ba348460f973e8ec90fb71f91f93120ae4f2ac0
Tran Anh Vu (vuwin10) <anvut20@gmail.com>
2023-12-28 09:33:46 +0700

    small err when installing

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -e
 git clone https://github.com/uit-anhvuk13/VulDetImp
 cd VulDetImp
 mkdir -p DATA/{FUN,CFG,RAW,WFG}
-for i in DATA/*; do mkdir -p $i/{APP,VUL,PAT}; done
+for i in DATA/*/; do mkdir -p $i/{APP,VUL,PAT}; done
 git clone https://github.com/leontsui1987/VulDetector
 rm -rf VulDetector/.git*
 cp -r src/DataPrepare VulDetector/
